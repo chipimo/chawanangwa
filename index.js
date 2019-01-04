c67d90aae8d8cf56811afdde11891d96e9c4f757
chipimo <chipimo31@gmail.com>
2019-01-04 09:11:17 +0200

    make it better

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use('/auth/auth', (req, res) => {
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'))
 })  
-
+const SocketManager = require('./SocketManager')
 io.on('connection', SocketManager)
 
 const PORT = process.env.PORT || 3200
