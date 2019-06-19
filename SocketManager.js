fc080eed365c6ae0c1e836b3421b925786911a5f
chipimo <chipimo31@gmail.com>
2019-06-19 09:10:40 +0200

    instail com

--- SocketManager.js
+++ SocketManager.js
@@ -131,7 +131,7 @@ module.exports = function(socket) {
       //     }
       //   });
       //   if (Users.length === num) {
-      // console.log(NewArr);  
+      // console.log(NewArr);
 
       io.to(callback.socketId).emit(ALL_USERS, callback);
       //   }
@@ -146,11 +146,32 @@ module.exports = function(socket) {
   });
 
   socket.on(USER_CONNECTED, user => {
-    user.socketId = socket.id;
-    connectedUsers = addUser(connectedUsers, user);
-    socket.user = user;
+    TempActiveUsers.push(user.token);
+    // var result = _(TempActiveUsers)
+    //   .differenceBy(
+    //     [
+    //       {
+    //         id: id.userId,
+    //         socketId: id.socketId,
+    //         name: id.name,
+    //         NotificationId: id.notiId
+    //       }
+    //     ],
+    //     "id"
+    //   )
+    //   .concat([
+    //     {
+    //       id: id.userId,
+    //       socketId: id.socketId,
+    //       name: id.name,
+    //       NotificationId: id.notiId
+    //     }
+    //   ])
+    //   .value();
+    // activeUsers = result;
+    console.log(TempActiveUsers);
 
-    io.emit(USER_CONNECTED, connectedUsers);
+    // io.emit(ACTIVE_USERS, result);
   });
 
   socket.on(VERIFY_FB_USER, user => {
@@ -161,7 +182,6 @@ module.exports = function(socket) {
 
     if (user.check) {
       _getUser_ById(userData, callback => {
-        
         if (callback.isSet) {
           bcrypt.compare(
             user.id,
@@ -263,6 +283,14 @@ module.exports = function(socket) {
                 icon: productData.img.url
               }
             });
+
+            SendNotfication({
+              body: "New product add (" + productData.data.name + ")",
+              data: {
+                name: productData.data.name,
+                icon: productData.img.url
+              }
+            });
           });
         });
       }
@@ -344,3 +372,85 @@ module.exports = function(socket) {
 };
 
 const addToUser = function(id, callback) {};
+
+const SendNotfication = function(data) {
+  for (let pushToken of TempActiveUsers) {
+    // Each push token looks like ExponentPushToken[xxxxxxxxxxxxxxxxxxxxxx]
+
+    // Check that all your push tokens appear to be valid Expo push tokens
+    if (!Expo.isExpoPushToken(pushToken)) {
+      console.error(`Push token ${pushToken} is not a valid Expo push token`);
+      continue;
+    }
+
+    // Construct a message (see https://docs.expo.io/versions/latest/guides/push-notifications.html)
+    messages.push({
+      to: pushToken,
+      sound: "default",
+      body: data.body,
+      data: { withSome: data.data }
+    });
+  }
+
+  let chunks = expo.chunkPushNotifications(messages);
+  let tickets = [];
+  (async () => {
+    // Send the chunks to the Expo push notification service. There are
+    // different strategies you could use. A simple one is to send one chunk at a
+    // time, which nicely spreads the load out over time:
+    for (let chunk of chunks) {
+      try {
+        let ticketChunk = await expo.sendPushNotificationsAsync(chunk);
+        console.log(ticketChunk);
+        tickets.push(...ticketChunk);
+        // NOTE: If a ticket contains an error code in ticket.details.error, you
+        // must handle it appropriately. The error codes are listed in the Expo
+        // documentation:
+        // https://docs.expo.io/versions/latest/guides/push-notifications#response-format
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  })();
+
+  let receiptIds = [];
+  for (let ticket of tickets) {
+    // NOTE: Not all tickets have IDs; for example, tickets for notifications
+    // that could not be enqueued will have error information and no receipt ID.
+    if (ticket.id) {
+      receiptIds.push(ticket.id);
+    }
+  }
+
+  let receiptIdChunks = expo.chunkPushNotificationReceiptIds(receiptIds);
+  (async () => {
+    // Like sending notifications, there are different strategies you could use
+    // to retrieve batches of receipts from the Expo service.
+    for (let chunk of receiptIdChunks) {
+      try {
+        let receipts = await expo.getPushNotificationReceiptsAsync(chunk);
+        console.log(receipts);
+
+        // The receipts specify whether Apple or Google successfully received the
+        // notification and information about an error, if one occurred.
+        for (let receipt of receipts) {
+          if (receipt.status === "ok") {
+            continue;
+          } else if (receipt.status === "error") {
+            console.error(
+              `There was an error sending a notification: ${receipt.message}`
+            );
+            if (receipt.details && receipt.details.error) {
+              // The error codes are listed in the Expo documentation:
+              // https://docs.expo.io/versions/latest/guides/push-notifications#response-format
+              // You must handle the errors appropriately.
+              console.error(`The error code is ${receipt.details.error}`);
+            }
+          }
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  })();
+};
